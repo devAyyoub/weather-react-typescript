c972f321a2134d4cc9629ad24e3aa6267db2436c
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-04-10 22:18:12 +0200

    added validation to the form

---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,8 @@
+import { PropsWithChildren } from "react";
+import styles from './Alert.module.css'
+
+export default function Alert({children} : PropsWithChildren) {
+  return (
+    <div className={styles.alert}>{children}</div>
+  )
+}
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,7 +1,8 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { countries } from "../../data/countries";
 import styles from './Form.module.css'
 import type { SearchType } from "../../types";
+import Alert from "../Alert/Alert";
 
 export default function Form() {
     const [search, setSearch] = useState<SearchType>({
@@ -9,14 +10,27 @@ export default function Form() {
         country: ''
     })
 
+    const [alert, setAlert] = useState('')
+
     const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
          setSearch({
             ...search,
             [e.target.name] : e.target.value
          })
     }
+
+    const handleSubmit = (e : FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        if(Object.values(search).includes('')) {
+            setAlert('Ningún campo puede estar vacío')
+            return
+        }
+    }
   return (
-    <form className={styles.form}>
+    
+    <form className={styles.form} onSubmit={handleSubmit}>
+        {alert && <Alert>{alert}</Alert>}
       <div className={styles.field}>
         <label htmlFor="city">Ciudad: </label>
         <input type="text" id="city" name="city" placeholder="Ciudad" value={search.city} onChange={handleChange}/>
