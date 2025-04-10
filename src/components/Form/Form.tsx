fdacbb81eb22967c07bd4cf80a002fbcc62e4479
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-04-10 22:04:14 +0200

    created state to store the city and country to be checked

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,16 +1,29 @@
+import { ChangeEvent, useState } from "react";
 import { countries } from "../../data/countries";
 import styles from './Form.module.css'
+import type { SearchType } from "../../types";
 
 export default function Form() {
+    const [search, setSearch] = useState<SearchType>({
+        city: '',
+        country: ''
+    })
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+         setSearch({
+            ...search,
+            [e.target.name] : e.target.value
+         })
+    }
   return (
     <form className={styles.form}>
       <div className={styles.field}>
         <label htmlFor="city">Ciudad: </label>
-        <input type="text" id="city" name="city" placeholder="Ciudad" />
+        <input type="text" id="city" name="city" placeholder="Ciudad" value={search.city} onChange={handleChange}/>
       </div>
       <div className={styles.field}>
         <label htmlFor="country">País: </label>
-        <select name="" id="">
+        <select name="country" id="country" value={search.country} onChange={handleChange}>
           <option>--Seleccione un país--</option>
           {countries.map((country) => (
             <option value={country.code} key={country.code}>
