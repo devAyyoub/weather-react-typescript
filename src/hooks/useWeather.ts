dc84c13ff6394222f60ef00bf42ad285ad7c9144
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-04-12 12:59:24 +0200

    created second api call to obtain a city weather

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -2,13 +2,21 @@ import axios from "axios";
 import { SearchType } from "../types";
 
 export default function useWeather() {
-  const fetchWeather = async (search : SearchType) => {
+  const fetchWeather = async (search: SearchType) => {
     try {
-        const apiKey = "9117dfd2d06047516ae5c7df98a160f9"
+      const apiKey = import.meta.env.VITE_API_KEY;
       const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${apiKey}`;
 
-      const data = await axios.get(geoUrl)
-      console.log(data)
+      const { data } = await axios.get(geoUrl);
+      const lat = data[0].lat;
+      const lon = data[0].lon;
+      console.log(lat);
+      console.log(lon);
+
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+      const { data: weatherResult } = await axios.get(weatherUrl);
+      console.log(weatherResult);
     } catch (error) {
       console.log(error);
     }
