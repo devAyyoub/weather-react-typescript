e6e0a7cf72c24f8e847b8901a58dab4f8ffda9a2
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-04-12 12:29:44 +0200

    first call to the weather api created

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -5,7 +5,7 @@ import type { SearchType } from "../../types";
 import Alert from "../Alert/Alert";
 
 type FormProps = {
-    fetchWeather: () => void 
+    fetchWeather: (search: SearchType) => Promise<void>
 }
 
 export default function Form({fetchWeather} : FormProps) {
@@ -30,6 +30,8 @@ export default function Form({fetchWeather} : FormProps) {
             setAlert('Ningún campo puede estar vacío')
             return
         }
+
+        fetchWeather(search)
     }
   return (
     
--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,9 +1,19 @@
-export default function useWeather() {
+import axios from "axios";
+import { SearchType } from "../types";
 
-    const fetchWeather = () => {
-        console.log('consultando...');       
+export default function useWeather() {
+  const fetchWeather = async (search : SearchType) => {
+    try {
+        const apiKey = "9117dfd2d06047516ae5c7df98a160f9"
+      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${apiKey}`;
+
+      const data = await axios.get(geoUrl)
+      console.log(data)
+    } catch (error) {
+      console.log(error);
     }
-    return {
-        fetchWeather
-    }
+  };
+  return {
+    fetchWeather,
+  };
 }
